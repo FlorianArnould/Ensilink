9a9c7d43de01d40904955e010be7e64714e9ad94
Arnould Florian <arnould@ecole.ensicaen.fr>
2016-12-14 18:36:59 +0100

    optimisation de la draweractivity

--- app/src/main/java/fr/ensicaen/lbssc/ensilink/DrawerActivity.java
+++ app/src/main/java/fr/ensicaen/lbssc/ensilink/DrawerActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,8 +20,14 @@ import fr.ensicaen.lbssc.ensilink.storage.School;
 import fr.ensicaen.lbssc.ensilink.storage.Union;
 import fr.ensicaen.lbssc.ensilink.unionscreen.UnionScreenActivity;
 
+/**
+ * @author Florian Arnould
+ * @version 1.0
+ */
 
-
+/**
+ * class which implement the toolbar and drawer for all activities
+ */
 public abstract class DrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -39,31 +46,16 @@ public abstract class DrawerActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         refreshDrawer();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
     private void refreshDrawer(){
         final Menu menu = ((NavigationView)findViewById(R.id.nav_view)).getMenu();
         menu.clear();
+        menu.add("Actualité").setCheckable(true);
         List<Union> list = School.getInstance().getUnions();
         for(int i=0;i<list.size();i++){
-            final int id = i;
-            menu.add(list.get(i).getName()).setCheckable(true).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem menuItem) {
-                    Intent intent = new Intent(DrawerActivity.this, UnionScreenActivity.class);
-                    intent.putExtra("UNION_ID", id);
-                    startActivity(intent);
-                    if(!(DrawerActivity.this instanceof MainActivity)){
-                        finish();
-                    }else{
-                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                        drawer.closeDrawer(GravityCompat.START);
-                        //TODO do this
-                        menuItem.setChecked(false);
-                    }
-                    return true;
-                }
-            });
+            menu.add(list.get(i).getName()).setCheckable(true);
         }
     }
 
@@ -102,6 +94,29 @@ public abstract class DrawerActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
+        if(item.toString().equals("Actualité")){
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            if(!(DrawerActivity.this instanceof MainActivity)){
+                finish();
+            }
+        }
+        List<Union> list = School.getInstance().getUnions();
+        for(int i=0;i<list.size();i++){
+            if(item.toString().equals(list.get(i).getName())){
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                Intent intent = new Intent(DrawerActivity.this, UnionScreenActivity.class);
+                intent.putExtra("UNION_ID", i);
+                startActivity(intent);
+                if(!(DrawerActivity.this instanceof MainActivity)){
+                    finish();
+                }else{
+                    //TODO do this
+                    item.setChecked(false);
+                }
+            }
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
