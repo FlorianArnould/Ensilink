55f585e649e26c7ad973edad73bc9efada187eff
Arnould Florian <arnould@ecole.ensicaen.fr>
2017-01-27 15:54:11 +0100

    Elimination du bug de la page des crédits

--- app/src/main/java/fr/ensicaen/lbssc/ensilink/view/creditsscreen/CreditsActivity.java
+++ app/src/main/java/fr/ensicaen/lbssc/ensilink/view/creditsscreen/CreditsActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,6 +31,8 @@ import fr.ensicaen.lbssc.ensilink.storage.School;
  */
 public final class CreditsActivity extends AppCompatActivity {
 
+    private CreditsAdapter _creditAdapter;
+
     @Override
     @SuppressWarnings("deprecation") //For retro compatibility
     protected void onCreate(Bundle savedStateInstance){
@@ -45,7 +48,8 @@ public final class CreditsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         ListView list = (ListView) findViewById(R.id.list);
-        list.setAdapter(new CreditsAdapter());
+        _creditAdapter = new CreditsAdapter();
+        list.setAdapter(_creditAdapter);
     }
 
     @Override
@@ -63,32 +67,22 @@ public final class CreditsActivity extends AppCompatActivity {
      */
     private final class CreditsAdapter extends BaseAdapter {
 
-        private final List<Integer> _rowLayout;
-        private final List<Object> _rowContent;
+        private List<Object> _rowContent;
 
         CreditsAdapter(){
-            _rowLayout = new ArrayList<>();
             _rowContent = new ArrayList<>();
-            _rowLayout.add(R.layout.credits_title_row);
             _rowContent.add(getString(R.string.developers));
-            _rowLayout.add(R.layout.credits_developer_row);
             String team[] = getResources().getStringArray(R.array.team_2016_2017);
             _rowContent.add(new Developer(R.drawable.ic_manager, getString(R.string.manager), team[0]));
             for(int i=1;i<team.length;i++){
-                _rowLayout.add(R.layout.credits_developer_row);
                 _rowContent.add(new Developer(R.drawable.ic_developer, getString(R.string.developer), team[i]));
             }
-            _rowLayout.add(R.layout.credits_title_row);
             _rowContent.add(getString(R.string.attributions));
-            _rowLayout.add(R.layout.credits_attribution_row);
             _rowContent.add(new Artist(getDrawable(R.mipmap.ic_launcher), getString(R.string.application_icon)));
-            _rowLayout.add(R.layout.credits_attribution_row);
             _rowContent.add(new Artist(getDrawable(R.drawable.ic_manager), getString(R.string.manager_icon)));
-            _rowLayout.add(R.layout.credits_attribution_row);
             _rowContent.add(new Artist(getDrawable(R.drawable.ic_developer), getString(R.string.developer_icon)));
             for(Image image : School.getInstance().getImages()){
                 if(image.needsAttribution()){
-                    _rowLayout.add(R.layout.credits_attribution_row);
                     _rowContent.add(new Artist(image));
                 }
             }
@@ -96,7 +90,7 @@ public final class CreditsActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return _rowLayout.size();
+            return _rowContent.size();
         }
 
         @Override
@@ -110,20 +104,38 @@ public final class CreditsActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup parent) {
-            if(view == null) {
-                LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-                view = inflater.inflate(_rowLayout.get(i), parent, false);
+        public int getItemViewType(int position){
+            Log.d("Debug", String.valueOf(position));
+            if(position == 0){
+                return 0;
+            }else if(position > 0 && position < 4){
+                return 1;
+            }else if(position == 4){
+                return 0;
+            }else{
+                return 2;
             }
+        }
+
+        @Override
+        public int getViewTypeCount(){
+            return 3;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             TextView text;
             ImageView image;
-            switch (_rowLayout.get(i)){
-                case R.layout.credits_title_row:
+            Log.d("Debug", String.valueOf(getItemViewType(i)));
+            switch (getItemViewType(i)){
+                case 0:
+                    view = inflater.inflate(R.layout.credits_title_row, parent, false);
                     text = (TextView) view.findViewById(R.id.title);
                     text.setText((String)_rowContent.get(i));
                     break;
-                case R.layout.credits_developer_row:
+                case 1:
+                    view = inflater.inflate(R.layout.credits_developer_row, parent, false);
                     Developer dev = (Developer) _rowContent.get(i);
                     image = (ImageView)view.findViewById(R.id.icon);
                     image.setImageResource(dev.getDrawableId());
@@ -132,13 +144,13 @@ public final class CreditsActivity extends AppCompatActivity {
                     text = (TextView)view.findViewById(R.id.name);
                     text.setText(dev.getName());
                     break;
-                case R.layout.credits_attribution_row:
+                default:
+                    view = inflater.inflate(R.layout.credits_attribution_row, parent, false);
                     Artist artist = (Artist) _rowContent.get(i);
                     image = (ImageView)view.findViewById(R.id.icon);
                     image.setImageDrawable(artist.getDrawable());
                     text = (TextView)view.findViewById(R.id.attribution);
                     text.setText(artist.getAttribution());
-                    break;
             }
             return view;
         }
