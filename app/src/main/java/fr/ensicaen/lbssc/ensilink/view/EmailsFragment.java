1ac0368cf2c85158e36ca987ef8fdf48ddc46711
Jérémy <filipozzi@ecole.ensicaen.Fr>
2017-03-26 16:08:27 +0200

    Mise à jour de la classe MailFragment et création de la classe Mail dans storage

--- app/src/main/java/fr/ensicaen/lbssc/ensilink/view/EmailsFragment.java
+++ app/src/main/java/fr/ensicaen/lbssc/ensilink/view/EmailsFragment.java
@@ -4,18 +4,24 @@ package fr.ensicaen.lbssc.ensilink.view;
 
 //TODO feature will be implemented in the next released
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
 
 import fr.ensicaen.lbssc.ensilink.R;
+import fr.ensicaen.lbssc.ensilink.storage.Mail;
 
 
 /**
  * @author Marsel Arik
- * @version 1.0
+ * @author Jérémy Filipozzi
+ * @version 2.0
  */
 
 /**
@@ -23,6 +29,8 @@ import fr.ensicaen.lbssc.ensilink.R;
  * The real implementation will come in the second version
  */
 public class EmailsFragment extends AssociationFragment {
+
+    private MailAdapter _adapter;
 
     /**
      * create an instance of InformationFragment
@@ -52,7 +60,46 @@ public class EmailsFragment extends AssociationFragment {
     }
 
     @Override
-    public void update() {
+    public void update() { /*IN LINE*/}
 
+    private final class MailAdapter extends BaseAdapter {
+
+        List<Mail> _mails;
+
+        /**
+         * Constructor of the class
+         * @param mails a list of mails
+         */
+        MailAdapter(List<Mail> mails){
+            super();
+            update(mails);
+        }
+
+        /**
+         * Replace the old list of mails by adding the new ones
+         * @param mails a list mails to add to the ListView
+         */
+        void update(List<Mail> mails){
+            _mails = mails;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount(){ return _mails.size(); }
+
+        @Override
+        public Object getItem(int i) { return _mails.get(i); }
+
+        @Override
+        public long getItemId(int i) { return 0; }
+
+        @Override
+        public View getView(int i, View view, ViewGroup parent) {
+            if(view == null){
+                LayoutInflater inflater = (LayoutInflater) EmailsFragment.this.getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.mails_union,parent,false);
+            }
+            return view;
+        }
     }
 }
