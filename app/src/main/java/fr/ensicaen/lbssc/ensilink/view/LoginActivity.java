02ada5e4782d7898c10f907cf6aec1fa57833d31
Jérémy <filipozzi@ecole.ensicaen.Fr>
2017-04-12 16:42:29 +0200

    Création d'une AsyncTask pour gérer la connexion

--- app/src/main/java/fr/ensicaen/lbssc/ensilink/view/LoginActivity.java
+++ app/src/main/java/fr/ensicaen/lbssc/ensilink/view/LoginActivity.java
@@ -15,114 +15,116 @@ import fr.ensicaen.lbssc.ensilink.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText _email;
-    private EditText _password;
-    private Session _session;
-    private Folder _folder;
-    private Store _store;
-    private static final String POP_SERVER3 = "zimbra.ensicaen.fr";
+        private EditText _email;
+        private EditText _password;
+        private Session _session;
+        private Folder _folder;
+        private Store _store;
+        private static final String POP_SERVER3 = "zimbra.ensicaen.fr";
 
-    /**
-     * Constructor of the class which initializes three private class parameters
-     */
-    public LoginActivity() {
-        this._session = null;
-        this._store = null;
-        this._folder = null;
-    }
+        /**
+         * Constructor of the class which initializes three private class parameters
+         */
+        public LoginActivity() {
+            this._session = null;
+            this._store = null;
+            this._folder = null;
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.login_screen_activity);
-        _email = (EditText) findViewById(R.id.input_email);
-        _password = (EditText) findViewById(R.id.input_password);
-        Button button = (Button) findViewById(R.id.login_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                new connection().execute();
-                Log.d("onCreate", "LOGIN");
+            setContentView(R.layout.login_screen_activity);
+            _email = (EditText) findViewById(R.id.input_email);
+            _password = (EditText) findViewById(R.id.input_password);
+            Button button = (Button) findViewById(R.id.login_btn);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    login();
+                    Log.d("onCreate", "LOGIN");
+                }
+            });
+        }
+
+        /**
+         * This method connects the user to zimbra thanks to his username and
+         * password through pop3, SSL and TLS
+         *
+         * @throws Exception
+         */
+        public void connect() throws Exception {
+            Properties pop3Properties = new Properties();
+            pop3Properties.setProperty("mail.pop3.ssl.enable", "true");
+            pop3Properties.setProperty("mail.pop3.starttls.enable", "true");
+            pop3Properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            pop3Properties.setProperty("mail.pop3.port", "995");
+            pop3Properties.setProperty("mail.pop3.socketFactory.port", "995");
+
+            String email = "filipozzi@ecole.ensicaen.fr";//_email.getText().toString();
+            String password = "JF070794jf";//_password.getText().toString();
+
+            _session = Session.getInstance(pop3Properties);
+            _store = _session.getStore(new URLName("pop3://" + POP_SERVER3));
+            _store.connect(email, password);
+        }
+
+        public void openFolder(String folderName) throws Exception {
+            _folder = _store.getDefaultFolder();
+            _folder = _folder.getFolder(folderName);
+
+            if (_folder == null) {
+                throw new Exception("Invalid Folder");
             }
-        });
-    }
-
-    /**
-     * This method connects the user to zimbra thanks to his username and
-     * password through pop3, SSL and TLS
-     *
-     * @throws Exception
-     */
-    public void connect() throws Exception {
-        Properties pop3Properties = new Properties();
-        pop3Properties.setProperty("mail.pop3.ssl.enable", "true");
-        pop3Properties.setProperty("mail.pop3.starttls.enable", "true");
-        pop3Properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        pop3Properties.setProperty("mail.pop3.port", "995");
-        pop3Properties.setProperty("mail.pop3.socketFactory.port", "995");
-
-        String email = "filipozzi@ecole.ensicaen.fr";//_email.getText().toString();
-        String password = "JF070794jf";//_password.getText().toString();
-
-        _session = Session.getInstance(pop3Properties);
-        _store = _session.getStore(new URLName("pop3://" + POP_SERVER3));
-        _store.connect(email, password);
-    }
-
-    public void openFolder(String folderName) throws Exception {
-        _folder = _store.getDefaultFolder();
-        _folder = _folder.getFolder(folderName);
-
-        if (_folder == null) {
-            throw new Exception("Invalide Folder");
-        }
-        try {
-            _folder.open(Folder.READ_WRITE);
-        } catch (MessagingException e) {
-            _folder.open(Folder.READ_ONLY);
-        }
-    }
-
-    private class connection extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            final ProgressDialog progressDialog = new ProgressDialog(this, R.style.AppTheme_Dark_Dialog);
-            progressDialog.setIndeterminate(true);
-            progressDialog.setMessage("Authentification ...");
-            super.onPreExecute();
+            try {
+                _folder.open(Folder.READ_WRITE);
+            } catch (MessagingException e) {
+                _folder.open(Folder.READ_ONLY);
+            }
         }
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            if (validate()) {
-                Log.d("login", "authentification");
-                //progressDialog.show();
+        private void login(){
+            if(validate()) {
+                final ProgressDialog progressDialog = new ProgressDialog(this,
+                        R.style.AppTheme_Dark_Dialog);
+                progressDialog.setIndeterminate(true);
+                progressDialog.setMessage("Authentification ...");
+                progressDialog.show();
+                progressDialog.cancel();
+            }
+        }
+
+        private boolean validate(){
+            boolean valid = true;
+            String email = _email.getText().toString();
+            String password = _password.getText().toString();
+            if(email.isEmpty() || !email.contains("@ecole.ensicaen.fr")){
+                _email.setError("Email de l'ensicaen");
+                valid = false;
+            }
+            if(password.isEmpty()){
+                valid = false;
+            }
+            return valid;
+        }
+
+        private class connection extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... params) {
                 LoginActivity zimbra = new LoginActivity();
                 try {
-                    Log.d("try", "CONNECT");
                     zimbra.connect();
                     zimbra.openFolder("INBOX");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                return null;
             }
-            return null;
         }
     }
 
-    private boolean validate(){
-        boolean valid = true;
-        String email = _email.getText().toString();
-        String password = _password.getText().toString();
-        if(email.isEmpty() || !email.contains("@ecole.ensicaen.fr")){
-            _email.setError("Email de l'ensicaen");
-            valid = false;
-        }
-        if(password.isEmpty()){
-            valid = false;
-        }
-        return valid;
-    }
-}
+
+
+
