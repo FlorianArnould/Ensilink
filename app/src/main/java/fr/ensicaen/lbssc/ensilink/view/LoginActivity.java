c73e8ed68fb54db347455016dfffe4db47cdebd7
Jérémy <filipozzi@ecole.ensicaen.Fr>
2017-04-12 15:51:04 +0200

    Intégration de la connection à zimbra

--- app/src/main/java/fr/ensicaen/lbssc/ensilink/view/LoginActivity.java
+++ app/src/main/java/fr/ensicaen/lbssc/ensilink/view/LoginActivity.java
@@ -1,11 +1,15 @@
 package fr.ensicaen.lbssc.ensilink.view;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import javax.mail.*;
+import java.util.*;
 
 import fr.ensicaen.lbssc.ensilink.R;
 
@@ -13,6 +17,19 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText _email;
     private EditText _password;
+    private Session _session;
+    private Folder _folder;
+    private Store _store;
+    private static final String POP_SERVER3 = "zimbra.ensicaen.fr";
+
+    /**
+     * Constructor of the class which initializes three private class parameters
+     */
+    public LoginActivity() {
+        this._session = null;
+        this._store = null;
+        this._folder = null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +41,74 @@ public class LoginActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.login_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                login();
+            public void onClick(View v){
+                new connection().execute();
+                Log.d("onCreate", "LOGIN");
             }
         });
     }
 
-    private void login(){
-        if(validate()) {
-            final ProgressDialog progressDialog = new ProgressDialog(this,
-                    R.style.AppTheme_Dark_Dialog);
+    /**
+     * This method connects the user to zimbra thanks to his username and
+     * password through pop3, SSL and TLS
+     *
+     * @throws Exception
+     */
+    public void connect() throws Exception {
+        Properties pop3Properties = new Properties();
+        pop3Properties.setProperty("mail.pop3.ssl.enable", "true");
+        pop3Properties.setProperty("mail.pop3.starttls.enable", "true");
+        pop3Properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        pop3Properties.setProperty("mail.pop3.port", "995");
+        pop3Properties.setProperty("mail.pop3.socketFactory.port", "995");
+
+        String email = "filipozzi@ecole.ensicaen.fr";//_email.getText().toString();
+        String password = "JF070794jf";//_password.getText().toString();
+
+        _session = Session.getInstance(pop3Properties);
+        _store = _session.getStore(new URLName("pop3://" + POP_SERVER3));
+        _store.connect(email, password);
+    }
+
+    public void openFolder(String folderName) throws Exception {
+        _folder = _store.getDefaultFolder();
+        _folder = _folder.getFolder(folderName);
+
+        if (_folder == null) {
+            throw new Exception("Invalide Folder");
+        }
+        try {
+            _folder.open(Folder.READ_WRITE);
+        } catch (MessagingException e) {
+            _folder.open(Folder.READ_ONLY);
+        }
+    }
+
+    private class connection extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            final ProgressDialog progressDialog = new ProgressDialog(this, R.style.AppTheme_Dark_Dialog);
             progressDialog.setIndeterminate(true);
             progressDialog.setMessage("Authentification ...");
-            progressDialog.show();
-            //TODO Authentication by Jeremy
-            //progressDialog.cancel();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (validate()) {
+                Log.d("login", "authentification");
+                //progressDialog.show();
+                LoginActivity zimbra = new LoginActivity();
+                try {
+                    Log.d("try", "CONNECT");
+                    zimbra.connect();
+                    zimbra.openFolder("INBOX");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
         }
     }
 
